7d8c73a54382eacdc1f7ee2169212893107eb9dc
Ayamin <ayamin1337@yahoo.com>
2016-04-02 13:24:26 -0400

    Query Manager salary parsing function

--- src/querymanager/QueryManager.java
+++ src/querymanager/QueryManager.java
@@ -8,12 +8,52 @@ import com.wolfram.alpha.WASubpod;
 public class QueryManager {
 
 	/**
-	 * Returns the dollar amount cost per year of college tuition
-	 * resulted from a particular query.
-	 * @param queryResult
-	 * @return
+	 * Parses a query result for of tuition from a college.
+	 * @param queryResult The result of a query of the form "college tuition for ${COLLEGE_NAME}".
+	 * @return The dollar amount of tuition for a particular college.
 	 */
 	public static int parseCollegeTuitionQuery(WAQueryResult queryResult) {
+		int tuition = 0;
+		if (queryResult.isError()) {
+			System.out.println("Query error");
+			System.out.println("  error code: " + queryResult.getErrorCode());
+			System.out.println("  error message: " + queryResult.getErrorMessage());
+		} else if (!queryResult.isSuccess()) {
+			System.out.println("Query was not understood; no results available.");
+		} else {
+			// Got a result.
+			System.out.println("Successful query. Pods follow:\n");
+			for (WAPod pod : queryResult.getPods()) {
+				if (!pod.isError()) {
+					// System.out.println(pod.getTitle());
+					if(pod.getTitle().equals("Result")) {
+							for (WASubpod subpod : pod.getSubpods()) {
+								for (Object element : subpod.getContents()) {
+									if (element instanceof WAPlainText) {
+										String plaintext_tuition = ((WAPlainText) element).getText();
+										if(plaintext_tuition.length() == 0 || plaintext_tuition.charAt(0) != '$') {
+											System.out.println("Error parsing tuition result."); 
+											break;
+										}
+										// Replaces all non-digits with the empty string.
+										tuition = Integer.parseInt(plaintext_tuition.replaceAll("[\\D]", ""));
+									}
+								}
+							}
+							break;
+					}
+				}
+			}
+		}
+		return tuition;
+	}
+	
+	/**
+	 * Parses a query result for the average salary of a profession.
+	 * @param queryResult The result of a query of the form "average salary in ${CITY} for ${PROFESSION}".
+	 * @return The dollar amount of salary for a particular profession.
+	 */
+	public static int parseSalaryQuery(WAQueryResult queryResult) {
 		int tuition = 0;
 		if (queryResult.isError()) {
 			System.out.println("Query error");
